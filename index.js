8e9ab60e526f38a3fbafe143a54a3e12d7b3d28f
Mohammed Al Mitwalli <mohammed.almitwalli@gmail.com>
2023-03-26 02:04:48 -0700

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sallyh', 'Annette', 'Sarrah', 'bobby']
+
+function findMatching (array, string){
+    let newArray = [];
+    for (let element of array){
+            if (element == string || element == string.toUpperCase() || element == string.toLowerCase()){
+            newArray.push(element);
+            }
+    }
+
+return newArray                     
+}
+findMatching(drivers, "Bobby");
+
+
+
+function fuzzyMatch (array, string){
+    let newArray = [];
+        for (let element of array){
+            if (string == element.slice(0,2)){
+            newArray.push(element);}
+        }
+return newArray
+}
+fuzzyMatch(drivers, "Sa");
+
+
+function matchName(array, string){
+    let newArray = [];
+    for (let element of array){
+       
+       if (element.name === string){
+          newArray.push(element);
+          
+        }
+      
+    }
+    return newArray
+}
+(matchName(drivers, "Bobby"));
+
